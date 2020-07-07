b865ed450ef3d4ba7b7276911c7d4ce8ce396799
мария пугачёва <mawytochka99@mail.ru>
2020-07-08 01:02:51 +0300

    Forbidden to press the submit button in the modal window

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const modalAdd = document.querySelector('.modal__add'),
-  addAd = document.querySelector('.add__ad');
+  addAd = document.querySelector('.add__ad'),
+  modalBtnSubmit = document.querySelector('.modal__btn-submit');
 
 addAd.addEventListener('click', () => {
   modalAdd.classList.remove('hide');
+  modalBtnSubmit.disabled = true;
 })
