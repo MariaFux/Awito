2efa6e05931255550504367e7e662161fefa33d0
мария пугачёва <mawytochka99@mail.ru>
2020-07-08 00:59:21 +0300

    Added hiding modal window

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const modalAdd = document.querySelector('.modal__add'),
+  addAd = document.querySelector('.add__ad');
+
+addAd.addEventListener('click', () => {
+  modalAdd.classList.remove('hide');
+})
