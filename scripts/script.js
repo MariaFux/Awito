fc5c95e49cd4ed5ce6410bb9bd55b52816b74294
мария пугачёва <mawytochka99@mail.ru>
2020-07-08 01:17:03 +0300

    Added const to help clear modal window

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,8 @@
 
 const modalAdd = document.querySelector('.modal__add'),
   addAd = document.querySelector('.add__ad'),
-  modalBtnSubmit = document.querySelector('.modal__btn-submit');
+  modalBtnSubmit = document.querySelector('.modal__btn-submit'),
+  modalSubmit = document.querySelector('.modal__submit');
 
 addAd.addEventListener('click', () => {
   modalAdd.classList.remove('hide');
